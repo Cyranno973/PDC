483f3ca49ab8f10bb9f11262345160cf074a11b6
Cyranno973 <gs.gouin@gmail.com>
2020-09-11 00:03:19 +0200

    class sauvegarder niamiquement

--- build - Copie/main.js
+++ build - Copie/main.js
@@ -5,12 +5,28 @@ const sum = document.getElementById("sum");
 let recuperationSum = sessionStorage.getItem("somme");
 let plusElt = document.getElementById('increment');
 let moinsElt = document.getElementById('decrement');
+let ulElt = document.getElementById('oa');
+let list = document.querySelectorAll('ul  li');
+let operator = document.querySelectorAll('.operator');
+let edit = document.querySelectorAll('.edit');
+let element;
+let name;
 
+for (let i = 0; i < list.length; i++) {
+	const element = list[i];
 
-plusElt.addEventListener('click', increment);
-moinsElt.addEventListener('click', decrement);
-pdc.addEventListener('click', reset);
-// si sessionstorage vide sum vaut 0  ou je recupere la valeur de sum dans le session storage
+	element.addEventListener('click', function (event) {
+		if (element.classList.contains('moins')) decrement(this);
+		if (element.classList.contains('editpax')) console.log('editpax');
+		if (element.classList.contains('editBebe')) console.log('editBebe');
+		if (element.classList.contains('plus')) console.log('plus');
+	})
+}
+// plusElt.addEventListener('click', increment);
+// moinsElt.addEventListener('click', decrement);
+// pdc.addEventListener('click', reset);
+
+//si sessionstorage vide sum vaut 0  ou je recupere la valeur de sum dans le session storage
 if (recuperationSum === null) {
 	sum.innerHTML = 0;
 	sessionStorage.setItem("somme", sum.textContent);
@@ -29,41 +45,52 @@ sum.addEventListener('focusout', function () {
 	recuperationSum = Number(sessionStorage.getItem("somme"));
 })
 
-function decrement() {
+function decrement(nameId) {
+
+	var nextNode = nameId.nextSibling;
+	nextNode = nextNode.nextSibling;
+	nextNode = nextNode.nextSibling;
+	nextNode = nextNode.nextSibling;
+	console.log(recuperationSum);
+
 	if (recuperationSum === null) {
-		sum.innerHTML = nbrpax-1;
-		sessionStorage.setItem("somme", sum.textContent);
+		console.log('helllo');
+		nextNode.innerHTML = nbrpax - 1;
+		sessionStorage.setItem(nextNode.classList[0], nextNode.innerHTML);
 		recuperationSum = Number(sessionStorage.getItem("somme"));
 	} else {
-		sum.innerHTML = recuperationSum-1;
-		sessionStorage.setItem("somme", sum.innerHTML);
-		recuperationSum =  Number(sessionStorage.getItem("somme"));
+		console.log(nextNode.classList[0]);
+		 recuperationSum = sessionStorage.getItem(nextNode.classList[0]);
+
+		nextNode.innerHTML = recuperationSum - 1;
+		sessionStorage.setItem(nextNode.classList[0], nextNode.innerHTML);
+		recuperationSum = Number(sessionStorage.getItem(nextNode.classList[0]));
 	}
 };
 
-function increment() {
-	if (recuperationSum === null) {
-		sum.innerHTML = nbrpax+1;
-		sessionStorage.setItem("somme", sum.innerHTML);
-		recuperationSum = Number(sessionStorage.getItem("somme"));
-	} else {
-		sum.innerHTML = recuperationSum + 1;
-		sessionStorage.setItem("somme", sum.innerHTML);
-		recuperationSum =  Number(sessionStorage.getItem("somme"));
-	}
-};
+// function increment() {
+// 	if (recuperationSum === null) {
+// 		sum.innerHTML = nbrpax+1;
+// 		sessionStorage.setItem("somme", sum.innerHTML);
+// 		recuperationSum = Number(sessionStorage.getItem("somme"));
+// 	} else {
+// 		sum.innerHTML = recuperationSum + 1;
+// 		sessionStorage.setItem("somme", sum.innerHTML);
+// 		recuperationSum =  Number(sessionStorage.getItem("somme"));
+// 	}
+// };
 
-function reset() {
-	sum.innerHTML = 0;
-	nbrpax = 0;
-	sessionStorage.setItem("somme", 0);
-	recuperationSum = Number(sessionStorage.getItem("somme"));
-};
+// function reset() {
+// 	sum.innerHTML = 0;
+// 	nbrpax = 0;
+// 	sessionStorage.setItem("somme", 0);
+// 	recuperationSum = Number(sessionStorage.getItem("somme"));
+// };
 
 
 
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("/pdc/sw.js")
-        .then(() => console.log("registered service worker!"));
-}
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker
+//         .register("/pdc/sw.js")
+//         .then(() => console.log("registered service worker!"));
+// }
